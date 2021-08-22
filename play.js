2d1d9d31ab0008fea0e67e9f3076a0c15b8a39d0
Hollie Hensman <hesnmanh@gmail.com>
2021-08-22 22:18:12 +0000

    Added handleUserInput to handle event data and implemented key command  ctrl+c to exit game mode

--- play.js
+++ play.js
@@ -1,3 +1,20 @@
 const { connect } = require("./client");
  // code that does something when the connection is first established
 const conn = connect();
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = (key) => {
+    process.stdout.write('.');
+    if (key === '\u0003') {
+      process.exit();
+    }
+    console.log(key);
+  }
+  process.stdin.on('data', handleUserInput);
+  
+  return stdin;
+}
+setupInput();
