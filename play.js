475a252a0dde015dfd929491d23ec80453d72f3c
Hollie Hensman <hesnmanh@gmail.com>
2021-08-22 22:30:33 +0000

    Imports connect function and setupInput function

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
-const { connect } = require("./client");
- // code that does something when the connection is first established
-const conn = connect();
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  const handleUserInput = (key) => {
-    process.stdout.write('.');
-    if (key === '\u0003') {
-      process.exit();
-    }
-    console.log(key);
-  }
-  process.stdin.on('data', handleUserInput);
-  
-  return stdin;
-}
+const { connect } = require('./client');
+const { setupInput } = require('./input');
+
+console.log('Connecting ...');
+connect();
+
 setupInput();
