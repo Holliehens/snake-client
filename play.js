5a83e2ea8ce9c1d91b396fb3a6cc13ab40a49d1f
Hollie Hensman <hesnmanh@gmail.com>
2021-08-22 17:13:17 +0000

    In client.js, export the connect function using ES6 Shorthand syntax. Import it in play.js.

--- play.js
+++ play.js
@@ -1,25 +1,3 @@
-const net = require("net");
+const { connect } = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-  });
-  conn.on("data", (data) => {
-  console.log(data.toString());
-  });
-
-
-
-
-  return conn;
-};
-
-console.log("Connecting ...");
 connect();
