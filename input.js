12eac82ed292e3dffbc3e9203d08068a6de8a28c
Hollie Hensman <hesnmanh@gmail.com>
2021-08-24 00:29:10 +0000

    Added 4 driections of movement to keys WASD

--- input.js
+++ input.js
@@ -1,7 +1,10 @@
-const { connect } = require("./client");
+let connection;
+
+//const { connect } = require("./client");
  // code that does something when the connection is first established
-const conn = connect();
-const setupInput = function() {
+//const conn = connect();
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -12,10 +15,24 @@ const setupInput = function() {
       process.exit();
     }
     console.log(key);
+    if (key === 'w') {
+    conn.write('Move: up');
+    }
+    if (key === 'a') {
+    conn.write('Move: left');
+    }
+    if (key === 's') {
+    conn.write('Move: down');
+    }
+    if (key === 'd') {
+    conn.write('Move: right');
+    console.log("HUNGRY!!");
+    }
+
   }
   process.stdin.on('data', handleUserInput);
   
   return stdin;
 }
-setupInput();
-module.exports = setupInput;
+
+module.exports = { setupInput };
